9cbad1896f0c7ee561a9aa11e9d0f3c8652464da
Wrktalk Admin <mahima.k@wrktalk.io>
2026-07-19 01:28:29 +0530

    added solution of Q 5

--- 5_day.sh
+++ 5_day.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
 # 5. Write a script that checks if a given string argument
 # is empty and prints an appropriate message either way. 
 # Concept: string tests. Hint: -z, -n, always quote "$1".
+
+
+str=$1
+
+if [[ -z "$str" ]]; then
+    echo "string empty"
+else
+    echo "string not empty"
+fi
+
+
+
+
